8b3eebd28572d0c104d929b21acb46d1f3778d02
panthamima <nastyadima2003@mail.ru>
2021-11-21 00:57:22 -0600

    manual&algorithm

--- main.c
+++ main.c
@@ -3,23 +3,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+char scan_save_word(char);
+char auto_complete(char);
+
 int main()
 {
-	FILE *fp;
+	FILE *lb;
 	char RU_ALPHABET[] = "абвгдежзийклмнопрстуфхцчшщъьыэюя";
 	char US_ALPHABET[] = "abcdefghijknmlopqrstuvwxyz";
 
-	char name[] = "LetterBase.txt";
-	if (getchar() = RU_ALPHABET)
-		fp = fopen(name, "a");
+	// char name[] = "LetterBase.txt";
+	if (getchar())
+		lb = fopen("LetterBase.txt", "a");
 	else 
 		printf("undeclarated char");
 
-	fclose(fp);
+	fclose(lb);
 	getchar();
 	return 0;
+}
 
 
-
-
+char scanWord(char) 
+{
+	FILE *file;
+    struct food {
+        char name[20]; 
+        unsigned qty; 
+        float price; 
+    };
+    struct food shop[10];
+    char i=0;
+ 
+    file = fopen("fscanf.txt", "r");
+ 
+    while (fscanf(file, "%s%u%f", 
+        shop[i].name, &(shop[i].qty), 
+        &(shop[i].price)) != EOF) {
+ 
+        printf("%s %u %.2f\n", shop[i].name, 
+               shop[i].qty, shop[i].price); 
+        i++;
+    }
 }
