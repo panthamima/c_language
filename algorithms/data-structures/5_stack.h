931e58d5e8e19d31dfbd7c9e9e3a03707721a1ce
panthamima <nastyadima2003@mail.ru>
2021-12-31 23:47:27 +0700

    add stack.h, stack.c(uncomplete)

---
+++ algorithms/data-structures/5_stack.h
@@ -0,0 +1,29 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack {
+    int top;
+    void **data;
+    int MAX;
+    void (*push)(struct Stack*, void*);
+    void* (*pop)(struct Stack*);
+    void (*display)(struct Stack*);
+    void* (*peek)(struct Stack*);
+    bool (*isEmpty)(struct Stack*);
+};
+
+typedef struct Stack Stack;
+
+Stack* newStack(int);
+void init_stack(Stack*, int);
+
+void (*push)(Stack*, void *);
+void* (*pop)(Stack*);
+void (*display)(Stack*);
+void* (*peek)(Stack*);
+bool (*isEmpty)(Stack*);
+
+#endif
