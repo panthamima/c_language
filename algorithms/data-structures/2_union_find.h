ca56edd136699912c2a7494b70bb233d29d1fc94
panthamima <nastyadima2003@mail.ru>
2021-12-31 23:45:36 +0700

    add union_find

---
+++ algorithms/data-structures/2_union_find.h
@@ -0,0 +1,20 @@
+#ifndef UNION_FIND
+#define UNION_FIND
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct union_find {
+    int size;
+    int* parent;
+} UnionFind;
+
+UnionFind* createUnionFind(int size);
+
+bool sameSet(UnionFind* uf, int a, int b);
+int getRoot(UnionFind* uf, int i);
+void merge(UnionFind* uf, int a, int b);
+void printUnionFind(UnionFind* uf);
+
+#endif
